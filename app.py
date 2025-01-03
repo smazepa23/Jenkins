c85c55e74366a1cfb663b5a341656c482834ac19
smazepa23 <s.mazepa@aqua-cloud.io>
2025-01-03 14:53:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+def generate_message():
+    return "Witaj, świecie!"
+
 def main():
-    message = "Witaj, świecie!"
+    message = generate_message()
     print(message)
     with open("raport.html", "w") as file:
         file.write(f"<html><body><h1>{message}</h1></body></html>")
 
 if __name__ == "__main__":
     main()
+
