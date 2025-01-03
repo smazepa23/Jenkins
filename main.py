1f0a1b4c1ba77775d80486704e1ed782b8bec1c5
smazepa23 <s.mazepa@aqua-cloud.io>
2025-01-03 14:11:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def main():
-    print("Witaj, świecie!")
+    message = "Witaj, świecie!"
+    print(message)
+    with open("raport.html", "w") as file:
+        file.write(f"<html><body><h1>{message}</h1></body></html>")
 
 if __name__ == "__main__":
     main()
