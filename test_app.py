8c104353c2a93c810221e1587bc5c3edcaf8ba99
smazepa23 <s.mazepa@aqua-cloud.io>
2025-01-07 10:40:17 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,10 +4,9 @@ from app import generate_message
 
 class TestApp(unittest.TestCase):
     def test_generate_message(self):
-        expected_message = "Witaj, świecie!"
+        expected_message = "hello world"
         self.assertEqual(generate_message(), expected_message)
 
 if __name__ == "__main__":
     with open('test-reports/results.xml', 'w') as output:
         unittest.main(testRunner=XMLTestRunner(output=output))
-
